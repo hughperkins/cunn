9e03c99f62f905f10922395d80fb38ec42ffc8bd
Aiden Nibali <dismaldenizen@gmail.com>
2016-03-10 11:22:21 +1100

    In-place ELU

--- lib/THCUNN/ELU.cu
+++ lib/THCUNN/ELU.cu
@@ -14,11 +14,36 @@ struct ELUupdateOutput_functor
   }
 };
 
-void THNN_CudaELU_updateOutput(THCState *state, THCudaTensor *input, THCudaTensor *output, float alpha)
+// in-place variant
+struct ELUupdateOutputIP_functor
 {
-  THAssert(THCudaTensor_checkGPU(state, 2, input, output));
-  THCudaTensor_resizeAs(state, output, input);
-  THCudaTensor_pointwiseApply2(state, output, input, ELUupdateOutput_functor(alpha));
+  const float alpha_;
+
+  ELUupdateOutputIP_functor(float alpha)
+    : alpha_(alpha)
+  {}
+
+  __device__ void operator()(float *x) const
+  {
+    *x = *x <= 0 ? (exp(*x) - 1) * alpha_ : *x;
+  }
+};
+
+void THNN_CudaELU_updateOutput(THCState *state, THCudaTensor *input, THCudaTensor *output,
+  float alpha, bool inplace)
+{
+  THCUNN_assertSameGPU(state, 2, input, output);
+
+  if (inplace)
+  {
+    THCudaTensor_pointwiseApply1(state, input, ELUupdateOutputIP_functor(alpha));
+    THCudaTensor_set(state, output, input);
+  }
+  else
+  {
+    THCudaTensor_resizeAs(state, output, input);
+    THCudaTensor_pointwiseApply2(state, output, input, ELUupdateOutput_functor(alpha));
+  }
 }
 
 struct ELUupdateGradInput_functor
@@ -35,10 +60,33 @@ struct ELUupdateGradInput_functor
   }
 };
 
-void THNN_CudaELU_updateGradInput(THCState *state, THCudaTensor *input, THCudaTensor *gradOutput,
-  THCudaTensor *gradInput, THCudaTensor *output, float alpha)
+struct ELUupdateGradInputIP_functor
 {
-  THAssert(THCudaTensor_checkGPU(state, 3, output, gradOutput, gradInput));
-  THCudaTensor_resizeAs(state, gradInput, output);
-  THCudaTensor_pointwiseApply3(state, gradInput, output, gradOutput, ELUupdateGradInput_functor(alpha));
+  const float alpha_;
+
+  ELUupdateGradInputIP_functor(float alpha)
+    : alpha_(alpha)
+  {}
+
+  __device__ void operator()(float *gradOutput, const float *output) const
+  {
+    *gradOutput = (*output) <= 0 ? (*gradOutput * (*output + alpha_)) : (*gradOutput);
+  }
+};
+
+void THNN_CudaELU_updateGradInput(THCState *state, THCudaTensor *input, THCudaTensor *gradOutput,
+  THCudaTensor *gradInput, THCudaTensor *output, float alpha, bool inplace)
+{
+  THCUNN_assertSameGPU(state, 3, output, gradOutput, gradInput);
+
+  if (inplace)
+  {
+    THCudaTensor_pointwiseApply2(state, gradOutput, output, ELUupdateGradInputIP_functor(alpha));
+    THCudaTensor_set(state, gradInput, gradOutput);
+  }
+  else
+  {
+    THCudaTensor_resizeAs(state, gradInput, output);
+    THCudaTensor_pointwiseApply3(state, gradInput, output, gradOutput, ELUupdateGradInput_functor(alpha));
+  }
 }
